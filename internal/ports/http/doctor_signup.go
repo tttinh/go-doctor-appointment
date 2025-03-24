7e4e8f666d314ffa5358f3f8c2abaceb29a78b35
tinhtt <tinhtrantrung.vn@gmail.com>
2025-03-25 00:07:13 +1300

    Design API contract.

---
+++ internal/ports/http/doctor_signup.go
@@ -0,0 +1,7 @@
+package httpport
+
+import "github.com/gin-gonic/gin"
+
+func (h handler) createDoctor(c *gin.Context) {
+
+}
--- internal/ports/http/server.go
+++ internal/ports/http/server.go
@@ -24,17 +24,22 @@ func (h handler) makeRouter() http.Handler {
 	router.Use(gin.Logger(), gin.Recovery())
 	api := router.Group("/api")
 
+	// Public resources.
+	api.GET("/doctor/:id/calendar", h.todo)
+
+	// Private resources.
 	// Doctors operations
 	api.POST("/doctor/signup", h.todo)
-	api.POST("/doctor/schedule", h.todo)
+	api.POST("/doctor/signin", h.todo)
+	api.POST("/doctor/slot", h.todo)
+	api.PUT("/doctor/slot/:id", h.todo)
 	api.GET("/doctor/appointment", h.todo)
-	api.GET("/doctor/:id/appointment", h.todo)
-	api.GET("/doctor/:id/calendar", h.todo)
 
 	// Patients operations
 	api.POST("/patient/signup", h.todo)
-	api.POST("/patient/appointment", h.todo)
+	api.POST("/patient/signin", h.todo)
 	api.GET("/patient/appointment", h.todo)
+	api.POST("/patient/appointment", h.todo)
 
 	return router
 }
